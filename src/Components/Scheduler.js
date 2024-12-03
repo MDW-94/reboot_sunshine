06ed909a8a4e683d81e322cda6cf672c12d281ed
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-12-03 12:27:16 +0000

    feat: added schedule

--- src/Components/Scheduler.js
+++ src/Components/Scheduler.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
@@ -7,9 +7,24 @@ const Scheduler = () => {
 
   return (
     <div>
-      <h2>Select a Date</h2>
-      <Calendar onChange={setSelectedDate} value={selectedDate} />
-      <p>Selected Date: {selectedDate.toDateString()}</p>
+      <div>
+        <h2>Select a Date</h2>
+        <Calendar onChange={setSelectedDate} value={selectedDate} />
+        <p>Selected Date: {selectedDate.toDateString()}</p>
+      </div>
+
+      <div>
+        {selectedDate && (
+          <div>
+            <h3> Set A Task For: {selectedDate.toDateString()}</h3>
+            <div>
+              <input placeholder="No."></input>
+              <input placeholder="Task"></input>
+              <input placeholder="Time"></input>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
